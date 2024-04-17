043cf516c07d24b6fa6a986d28c5fb1f1f801fee
mariolemes <mls.jnr@gmail.com>
2024-04-17 19:54:15 -0300

    ikjkk

--- paginaWeb/script.js
+++ paginaWeb/script.js
@@ -1,6 +1,10 @@
-var err = false; // Variável para indicar falha na produção
-var sensorStates = []; // Array para armazenar os estados do sensor ao longo do tempo
-var errDisplay = document.getElementById('errDisplay'); // Elemento HTML para exibir o valor de err
+const ligarButton = document.getElementById('ligarButton');
+const desligarButton = document.getElementById('desligarButton');
+const restartButton = document.getElementById('restartButton');
+const producaoDisplay = document.getElementById('producaoDisplay');
+
+var urlPost = 'https://leanwebsensor-svr.onrender.com/chaves'
+var urlGet = 'https://leanwebsensor-svr.onrender.com/producao'
 
 function receiverRequest(){
     fetch(urlGet, {
@@ -11,32 +15,35 @@ function receiverRequest(){
     })
     .then(response => response.json())
     .then(json => {
-        // Adiciona o estado atual do sensor ao array
-        sensorStates.push(json.sensor);
-
-        // Mantém o array com no máximo 10 elementos
-        if (sensorStates.length > 10) {
-            sensorStates.shift(); // Remove o primeiro elemento (o mais antigo)
-        }
-
-        // Verifica se todos os estados no array são iguais
-        const allEqual = arr => arr.every(val => val === arr[0]);
-
-        // Se todos os estados forem iguais e a planta estiver ligada, define err como true
-        if (allEqual(sensorStates) && json.ligada === 1) {
-            err = true;
-        } else {
-            err = false;
-        }
-
-        // Atualiza os elementos de exibição conforme necessário
         producaoDisplay.textContent = json.sensor;
         feedbackDisplay.textContent = json.msg;
         console.log(json.sensor);
-
-        // Atualiza o elemento HTML com o valor de err
-        errDisplay.textContent = err ? "Erro na produção" : "";
     })
 }
 
-setInterval(receiverRequest, 2000);
+setInterval(receiverRequest, 2000)  
+
+ligarButton.addEventListener('click', () => {
+    let requestData = {"liga": 1, "desliga": 0, "restart": 0}
+    sendRequest(requestData)
+});
+
+desligarButton.addEventListener('click', () => {
+    let requestData = {"liga": 0, "desliga": 1, "restart": 0}
+    sendRequest(requestData)
+});
+
+restartButton.addEventListener('click', () => {
+    let requestData = {"liga": 0, "desliga": 0, "restart": 1}
+    sendRequest(requestData)
+});
+
+function sendRequest(data){
+    fetch(urlPost, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
